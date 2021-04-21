71fbfbb2b4b31433d2f578ecde948e21e239db4a
Edu <61661507+edu-bio@users.noreply.github.com>
2021-04-21 14:37:33 +0200

    Comunicando con el micro NodeJs-Express-TypeScript original

---
+++ app.js
@@ -0,0 +1,64 @@
+
+var express 	= require("express"); 
+var app 	= express();
+var bodyParser 	= require('body-parser');
+var request 	= require("request");
+
+// URL con contenido JSON
+var url = "http://localhost:3001/libros/"
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
+ 
+//GET http://localhost:3002/books
+// Consumimos datos de la URL
+app.get('/books', function(req, res) {
+	request({
+	    url: url,
+	    json: false
+	}, function (error, response, body) {
+
+	    if (!error && response.statusCode === 200) {
+	    	// Pintamos la respuesta JSON en navegador.
+	        res.send(body) 
+	    }
+	})
+});
+ 
+//GET http://localhost:3002/books/3
+app.get('/books/:id', function(req, res) {
+
+	var libroId = req.params.id;
+
+	request({
+	    url: url+libroId,
+	    json: false
+	}, function (error, response, body) {
+
+	    if (!error && response.statusCode === 200) {
+	    	// Pintamos la respuesta JSON en navegador.
+	        res.send(body) 
+	    }
+	})
+})
+
+//Create http://localhost:3002/books/
+app.get('/books', function(req, res) {
+
+    var libro = req.params;
+
+	request({
+	    url: url,
+	    json: false
+	}, function (error, response) {
+
+	    if (!error && response.statusCode === 200) {
+	    	// Pintamos la respuesta JSON en navegador.
+	        res.send('libro creado con éxito') 
+	    }
+	})
+})
+  
+var server = app.listen(puerto = 3002, function () {
+    console.log(`Servidor en puerto ${puerto}..`); 
+});
---
+++ index.js
@@ -0,0 +1,5 @@
+/*dentro del package.json dentro del apartado de scripts he creado uno que se llama "start" y ejecuta el siguiente comando "node index.js",
+de modo que si quiero ejecutar el proyecto puedo hacerlo de dos maneras:
+-En el terminal poniendo: node index.js
+-En el terminal poniendo: npm run <nombre_del_script> --> en este caso npm run start*/
+console.log("Hola Mundo, probando...")
